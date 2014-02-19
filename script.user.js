08f30ff76290f451dcb40507ba8ca5ced8f89f2e
malaguer <c9cad37d@opayq.com>
2014-02-19 20:44:28 +0100

    Evitar bloqueo de diarios El Pais de otros países.
    Modificada línea 'elpais.com' para evitar bloquear a 'elpais.com.sv' y eliminada 'elpais.com.uy' que creo que no forma parte del grupo PRISA.

--- script.user.js
+++ script.user.js
@@ -1,158 +1,368 @@
 // ==UserScript==
-// @name anti AEDE
-// @namespace http://www.meneame.net/
-// @version 0.3.2
-// @description marcar en rojo
-// @include *
-// @updateURL http://raw.github.com/malaguer/anti-AEDE/master/script.js
-// @downloadURL http://raw.github.com/malaguer/anti-AEDE/master/script.js
-// @copyright Antonio Fernández Porrúa. Liberado bajo los términos de GPL
+// @name       anti AEDE
+// @namespace   http://www.meneame.net/
+// @version     0.7.2
+// @description  marcar en rojo
+// @include     *
+// @updateURL   https://raw.github.com/pykiss/anti-AEDE/master/script.user.js
+// @copyright   Antonio Fernández Porrúa. Pau Capó. Licencia     GPL
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @grant      GM_log
 // ==/UserScript==
 
-var aede = [
-/(\/|\.)abc\.es\//,
-/(\/|\.)aede\.es\//,
-/(\/|\.)as\.com\//,
-/(\/|\.)canarias7\.es\//,
-/(\/|\.)cincodias\.com\//,
-/(\/|\.)dbalears\.cat\//,
-/(\/|\.)deia\.com\//,
-/(\/|\.)diaridegirona\.cat\//,
-/(\/|\.)diaridetarragona\.com\//,
-/(\/|\.)diarideterrassa\.es\//,
-/(\/|\.)diariocordoba\.com\//,
-/(\/|\.)diariodeavila\.es\//,
-/(\/|\.)diariodeavisos\.com\//,
-/(\/|\.)diariodeburgos\.es\//,
-/(\/|\.)diariodecadiz\.es\//,
-/(\/|\.)diariodeibiza\.es\//,
-/(\/|\.)diariodejerez\.es\//,
-/(\/|\.)diariodelaltoaragon\.es\//,
-/(\/|\.)diariodeleon\.es\//,
-/(\/|\.)diariodemallorca\.es\//,
-/(\/|\.)diariodenavarra\.es\//,
-/(\/|\.)diariodenoticias\.org\//,
-/(\/|\.)diariodesevilla\.es\//,
-/(\/|\.)diarioinformacion\.com\//,
-/(\/|\.)diariojaen\.es\//,
-/(\/|\.)diariopalentino\.es\//,
-/(\/|\.)diariosur\.es\//,
-/(\/|\.)diariovasco\.com\//,
-/(\/|\.)eladelantado\.com\//,
-/(\/|\.)elalmeria\.es\//,
-/(\/|\.)elcomercio\.es\//,
-/(\/|\.)elcorreoweb\.es\//,
-/(\/|\.)elcorreo\.com\//,
-/(\/|\.)eldiadecordoba\.es\//,
-/(\/|\.)eldiariomontanes\.es\//,
-/(\/|\.)eleconomista\.es\//,
-/(\/|\.)elmundo\.es\//,
-/(\/|\.)elnortedecastilla\.es\//,
-/(\/|\.)elpais\.com\.uy\//,
-/(\/|\.)elmun\.do\//,
-/(\/|\.)elpais\.com\//,
-/(\/|\.)elpais\.es\//,
-/(\/|\.)elperiodicodearagon\.com\//,
-/(\/|\.)elperiodicoextremadura\.com\//,
-/(\/|\.)elperiodicomediterraneo\.com\//,
-/(\/|\.)elperiodico\.cat\//,
-/(\/|\.)elperiodico\.com\//,
-/(\/|\.)elprogreso\.es\//,
-/(\/|\.)europasur\.es\//,
-/(\/|\.)expansion\.com\//,
-/(\/|\.)farodevigo\.es\//,
-/(\/|\.)galiciae\.com\//,
-/(\/|\.)granadahoy\.com\//,
-/(\/|\.)heraldodesoria\.es\//,
-/(\/|\.)heraldo\.es\//,
-/(\/|\.)hoy\.es\//,
-/(\/|\.)huelvainformacion\.es\//,
-/(\/|\.)ideal\.es\//,
-/(\/|\.)intereconomia\.com\//,
-/(\/|\.)lagacetadesalamanca\.es\//,
-/(\/|\.)laopinioncoruna\.es\//,
-/(\/|\.)laopiniondemalaga\.es\//,
-/(\/|\.)laopiniondemurcia\.es\//,
-/(\/|\.)laopiniondezamora\.es\//,
-/(\/|\.)laopinion\.es\//,
-/(\/|\.)laprovincia\.es\//,
-/(\/|\.)larazon\.es\//,
-/(\/|\.)larioja\.com\//,
-/(\/|\.)lasprovincias\.es\//,
-/(\/|\.)latribunadealbacete\.es\//,
-/(\/|\.)latribunadeciudadreal\.es\//,
-/(\/|\.)latribunadetalavera\.es\//,
-/(\/|\.)latribunadetoledo\.es\//,
-/(\/|\.)lavanguardia\.com\//,
-/(\/|\.)laverdad\.es\//,
-/(\/|\.)lavozdealmeria\.es\//,
-/(\/|\.)lavozdegalicia\.es\//,
-/(\/|\.)lavozdigital\.es\//,
-/(\/|\.)levante-emv\.com\//,
-/(\/|\.)lne\.es\//,
-/(\/|\.)majorcadailybulletin\.es\//,
-/(\/|\.)malagahoy\.es\//,
-/(\/|\.)marca\.com\//,
-/(\/|\.)menorca\.info\//,
-/(\/|\.)mundodeportivo\.com\//,
-/(\/|\.)noticiasdealava\.com\//,
-/(\/|\.)noticiasdegipuzkoa\.com\//,
-/(\/|\.)regio7\.cat\//,
-/(\/|\.)sport\.es\//,
-/(\/|\.)superdeporte\.es\//,
-/(\/|\.)ultimahora\.es\//]
-;
+
+$(function () {
+
+   var aede = [
+      '11870.com',
+      '11824.es',
+      'abc.es',
+      'adn.fm',
+      'adnradio.cl',
+      'aede.es',
+      'alfaguara.com',
+      'as.com',
+      'autocasion.com',
+      'avanzaentucarrera.com',
+      'besame.fm',
+      'besame.co.cr',
+      'besame.com.mx',
+      'cadenadial.com',
+      'cadenaser.com',
+      'canalplus.es',
+      'canarias7.es',
+      'caracol.com.co',
+      'caracol1260.com',
+      'castellolopesmultimedia.com',
+      'cincodias.com',
+      'cinemania.es',
+      'clix.pt',
+      'colorincolorradio.com',
+      'concierto.cl',
+      'continental.com.ar',
+      'corazon.cl',
+      'correofarmaceutico.com',
+      'dalealplay.com',
+      'dbalears.cat',
+      'deia.com',
+      'diaridegirona.cat',
+      'diaridetarragona.com',
+      'diarideterrassa.es',
+      'diariocordoba.com',
+      'diariodeavila.es',
+      'diariodeavisos.com',
+      'diariodeburgos.es',
+      'diariodecadiz.es',
+      'diariodeibiza.es',
+      'diariodejerez.es',
+      'diariodelaltoaragon.es',
+      'diariodeleon.es',
+      'diariodemallorca.es',
+      'diariodenavarra.es',
+      'diariodenoticias.org',
+      'diariodesevilla.es',
+      'diarioinformacion.com',
+      'diariojaen.es',
+      'diariomedico.com',
+      'diariopalentino.es',
+      'diariosur.es',
+      'diariovasco.com',
+      'dmedicina.com',
+      'editorialaurus.com',
+      'eladelantado.com',
+      'elalmeria.es',
+      'elboomeran.com',
+      'elcomercio.es',
+      'elcorreoweb.es',
+      'elcorreo.com',
+      'eldiadecordoba.es',
+      'eldiariomontanes.es',
+      'eleconomista.es',
+      'elmundo.es',
+      'elnortedecastilla.es',
+      'elmun.do',
+      'elpais.com/',
+      'elpais.es',
+      'elpaisaguilar.es',
+      'elpaisclubdevinos.es',
+      'elperiodicodearagon.com',
+      'elperiodicoextremadura.com',
+      'elperiodicomediterraneo.com',
+      'elperiodico.cat',
+      'elperiodico.com',
+      'elpokerdeas.com',
+      'elprogreso.es',
+      'escolasdevalor.com.br',
+      'escuelaunidadeditorial.com.br',
+      'essayandscience.com',
+      'europasur.es',
+      'expansion.com',
+      'expansionyempleo.com',
+      'farodevigo.es',
+      'finanzas.com',
+      'fmdos.cl',
+      'fundacaosantillana.com',
+      'fundacaosantillana.com.br',
+      'fundacaosantillana.org.co',
+      'galiciae.com',
+      'globaliza.com',
+      'grada360.com',
+      'gruposantillanapr.com',
+      'granadahoy.com',
+      'heraldodesoria.es',
+      'heraldo.es',
+      'hjck.com',
+      'hoy.es',
+      'hoycinema.es',
+      'hoymotor.es',
+      'huelvainformacion.es',
+      'huffingtonpost.es',
+      'iarc.cl',
+      'iol.pt',
+      'ign.com',
+      'ideal.es',
+      'infoempleo.es',
+      'intereconomia.com',
+      'inverycrea.net',
+      'kebuena.com.mx',
+      'lavallenata.com',
+      'lagacetadesalamanca.es',
+      'laguiatv.com',
+      'lalistawip.com',
+      'laopinioncoruna.es',
+      'laopiniondemalaga.es',
+      'laopiniondemurcia.es',
+      'laopiniondezamora.es',
+      'laopinion.es',
+      'laprovincia.es',
+      'larazon.es',
+      'larioja.com',
+      'lasprovincias.es',
+      'latribunadealbacete.es',
+      'latribunadeciudadreal.es',
+      'latribunadetalavera.es',
+      'latribunadetoledo.es',
+      'lavanguardia.com',
+      'laverdad.es',
+      'lavozdealmeria.es',
+      'lavozdegalicia.es',
+      'lavozdigital.es',
+      'lasapuestasdeas.com',
+      'levante-emv.com',
+      'librosaguilarl.com',
+      'librosalfaguarainfantil.com',
+      'librosalfaguarajuvenil.com',
+      'lne.es',
+      'los40.cl',
+      'los40.com',
+      'los40.com.co',
+      'los40.com.cr',
+      'los40.com.ec',
+      'los40.com.gt',
+      'los40.com.mx',
+      'los40.com.pa',
+      'los40principales.com.ar',
+      'm80radio.com',
+      'majorcadailybulletin.es',
+      'malagahoy.es',
+      'marca.com',
+      'marcamotoranuncios.com',
+      'marcamotor.com',
+      'maxima.fm',
+      'mediacapital.pt',
+      'menorca.info',
+      'meristation.com',
+      'motormercado.com',
+      'mundodeportivo.com',
+      'mujerhoy.com',
+      'noticiasdealava.com',
+      'noticiasdegipuzkoa.com',
+      'objetiva.com.br',
+      'objetiva.pt',
+      'onstage.es',
+      'orbyt.tv',
+      'oxigeno.fm',
+      'pisos.com',
+      'planetevents.es',
+      'pluralent.com',
+      'pluralportugal.pt',
+      'plus.es',
+      'premiovivalectura.org.ar',
+      'premiovivaleitura.org.br',
+      'prisadigital.com',
+      'prisaediciones.com',
+      'prisalabs.com',
+      'prisanoticias.com',
+      'prisaradio.com',
+      'prisarevistas.com',
+      'prisatv.com',
+      'pudahuel.cl',
+      'puntodelectura.com',
+      'que.es',
+      'radioacktiva.cl',
+      'radioacktiva.com',
+      'radiocomercial.com',
+      'radioimagina.cl',
+      'radiole.com',
+      'radiounochile.cl',
+      'regio7.cat',
+      'richmondelt.com',
+      'rlm.es',
+      'rockandpop.cl',
+      'rollingstone.es',
+      'santillana.cl',
+      'santillana.com.ar',
+      'santillana.com.bo',
+      'santillana.com.br',
+      'santillana.com.co',
+      'santillana.com.do',
+      'santillana.com.ec',
+      'santillana.com.gt',
+      'santillana.com.hn',
+      'santillana.com.mx',
+      'santillana.com.pe',
+      'santillana.com.py',
+      'santillana.com.sv',
+      'santillana.com.uy',
+      'santillana.com.ve',
+      'santillana.com',
+      'santillana.cr',
+      'santillana.pt',
+      'santillanafrancais.com',
+      'santillanausa.com',
+      'seminariodenarrativayperiodismo.com',
+      'sistemauno.com',
+      'sport.es',
+      'sumadeletras.es',
+      'superdeporte.es',
+      'tareasymas.es',
+      'telva.com',
+      'tiramillas.net',
+      'tropicanafm.com',
+      'ultimahora.es',
+      'unidadeditorial.es',
+      'vadejuegos.com',
+      'vodafone.fm',
+      'vmetv.com',
+      'wradio.com.co',
+      'wradio.com.mx',
+      'wradio690.com',
+
+   ],
+      tooltip = $('<span id="aede-tooltip" style="position: absolute;display:none;background:#d04544;color:white;padding:5px;border-radius:4px;z-index:100000">AEDE alert!</span>'),
+
+      meneame = function(){
+         // Menéame
+         $('span.showmytitle').each(function (i) {
+            var title = this.title,
+               element = $(this).parents('.news-body');
+            preCheckAEDE(element, title, i);
+         });
+         $('input#url').bind('input', function () {
+            var that = $(this);
+            if(isAEDE(that.val())){
+               that.css('border', '2px solid red');
+            }else{
+               that.css('border', '1px solid #ddd');
+            }
+         });
+      
+      },
+      twitter = function(){
+         // Twitter by @Hanxxs http://pastebin.com/f04tPcsG
+         $('a.twitter-timeline-link').each(function (i) {
+            var title = this.title,
+               element = $(this).parents('.stream-item');
+            preCheckAEDE(element, title, i);
+         });
+      },
+      facebook = function(){
+         $('div.fsm').each(function (i) {
+            var title = $(this).text(),
+               element = $(this).parents('a.shareLink');
+            preCheckAEDE(element, title, i);
+         });
+         $('.userContent a').each(function (i) {
+            var title = $(this).attr('href'),
+               element = $(this);
+            preCheckAEDE(element, title, i);
+         });
+      },
+      others = function(){
+         // Others by @paucapo
+         $('a').each(function (i) {
+            var element = $(this),
+               href = element.attr('href');
+            preCheckAEDE(element, href, i);
+         });
+      },
+
+      checkForAEDELinks = function () {
+         switch(domain()){
+            case 'meneame.net':
+               meneame();
+            break;
+            case 'twitter.com':
+               twitter();
+            break;
+            case 'facebook.com':
+               facebook();
+            break;
+            default:
+               others();
+            break;
+         }
+      },
+      preCheckAEDE = function(element, url, i) {
+         if(url === undefined){
+            return;
+         }
+         setTimeout(function () {
+            checkAEDE(element, url);
+         }, i * 10);
+      },
+      checkAEDE = function(element, link) {
+         if (isAEDE(link)) {
+            element.css({
+               'background-color': '#ffe9e9',
+            })
+            /*.css({
+                   'background-image': 'linear-gradient(0deg, rgba(255,50,50,1),rgba(255,100,0,0.5))',
+                   'border-radius': '6px',
+                   'margin-bottom': '5px'
+                  })*/
+            .on('mouseenter', showTooltip).on('mouseleave', hideTooltip);
+         }
+      },
+      showTooltip = function () {
+         tooltip.show();
+      },
+      hideTooltip = function() {
+         tooltip.hide();
+      },
+      domain = function () {
+         var parts = document.domain.split('.');
+         return parts.slice(-2).join('.');
+      },
+      isAEDE = function(link) {
+         var is = false;
+         $.each(aede, function (i, a) {
+            if (link.indexOf(a) === 0 || link.indexOf('.' + a) >= 0 || link.indexOf('://' + a) >= 0) {
+               is = true;
+               return;
+            }
+         });
+         return is;
+      };
 
 
-$(function(){
-   var domain = document.domain;
-   if (domain == 'www.meneame.net' || domain== 'deportes.meneame.net' ) {
-      // Menéame
-      $('span.showmytitle').each(function(){
-         var thiss=this;
-         $.each(aede,function(i,regex){
-            if(regex.test(thiss.title)){
-               $(thiss).parents('.news-summary').css({
-                 'background-image': 'linear-gradient(0deg, rgba(255,50,50,1),rgba(255,100,0,0.5))',
-                 'border-radius': '6px',
-                 'margin-bottom': '5px'
-               });
-               return false;
-            }
-         });
-      });
-      $('input#url').keypress(function(){
-         var thi$ = this;
-         $.each(aede,function(i,regex){
-            if(regex.test(thi$.val ())){
-               thi$.css('border', '2px solid red');
-               return false;
-            }
-         });
-      });
-   } else if (domain == 'twitter.com' || domain == 'www.twitter.com') {
-      // Twitter
-      $('a.twitter-timeline-link').each(function(){
-         var thiss=this;
-         $.each(aede,function(i,regex){
-            if(regex.test(thiss.title)){
-               $(thiss).parents('.js-tweet-text').css('background', 'rgba(255,0,0,0.5)');
-               return false;
-            }
-         });
-      });
-   } else {
-      // Others
-      $('a').each(function() {
-         var link = $(this);
-         var href = link.attr('href');
-         var text = link.text();
-         $.each(aede,function(i,regex){
-            if(regex.test(href) || regex.test(text)) {
-               link.css('background', 'rgba(255,0,0,0.5)').css('color', 'white');
-               return false;
-            }
-        });
-      });
-   }
+
+   checkForAEDELinks();
+   setInterval(checkForAEDELinks, 2000);
+
+   $('body').append(tooltip);
+   $(document).mousemove(function (event) {
+      tooltip.css('top', (event.pageY + 10) + 'px').css('left', (event.pageX + 10) + 'px');
+   });
+
 });
